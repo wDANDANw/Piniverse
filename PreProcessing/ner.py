b31ad171aaaa24b4e364a6b9d069156fd2afd40b
alexandermkwan <75330113+alexandermkwan@users.noreply.github.com>
2023-01-16 22:08:01 -0500

    Add files via upload
    Signed-off-by: alexandermkwan <75330113+alexandermkwan@users.noreply.github.com>

---
+++ PreProcessing/ner.py
@@ -0,0 +1,21 @@
+import spacy
+from spacy import displacy
+from IPython.display import display, HTML
+
+NER = spacy.load("en_core_web_sm")
+
+raw_text = "There is four chair red laquer dining set shown in the image. There are opened white french doors leading " \
+           "to the outside showing. There is a pool with blue water showing through the french doors of The Indian Space Research Organisation. The pools is " \
+           "surrounded by green shrubbery.	The wood floor is covered with white paint. "
+
+text1 = NER(raw_text)
+
+
+for word in text1.ents:
+    print(word.text, word.label_)
+
+for word in text1:
+    print(word.lemma_, word.pos_, word.tag_, word.dep_,
+            word.shape_, word.is_alpha, word.is_stop)
+
+displacy.serve(text1, style="ent")
