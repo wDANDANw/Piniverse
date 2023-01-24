1d66df37293e698ce9ba388c6f3b2b8adf7e94f2
Azu109 <deshiming@outlook.com>
2023-01-24 17:54:28 -0500

    Enhance the appearance of Frontend

--- FrontendApplication/src/App.vue
+++ FrontendApplication/src/App.vue
@@ -1,20 +1,23 @@
 
 <template>
-  <v-app id="inspire">
-    <v-app-bar app>
-      <v-toolbar-title>Game Creation Engine</v-toolbar-title>
-    </v-app-bar>
-
+  <v-app id="inspire" align="center" style="background: #FFCCCC">
     <v-main>
-      <v-container fluid>
-        <v-col
-            cols="12"
-            md="6"
-            class="">
+<!--      align="center" style="background: #CCFFFF" cols="11"-->
+    <v-col md="11">
+<!--      Top of the page-->
+      <v-col align="center" style="background: #CCFFFF" class="rounded-xl mb-4">
+        <v-toolbar-title style="font-size: 30px; font-family: 'Comic Sans MS',serif">Game Creation Engine</v-toolbar-title>
+      </v-col>
+
+      <v-row class="pa-4">
+<!--        Left side of the page-->
+        <v-col style="background: #CCFFFF" class="rounded-xl mr-2">
+          <v-toolbar-title style="font-size: 20px; font-family: 'Comic Sans MS',serif">Parameters</v-toolbar-title>
           <v-textarea
-              name="input-7-1"
+              name="input"
               label="Input"
               v-model="input_str"
+              style="font-family: 'Comic Sans MS', serif"
           ></v-textarea>
           <v-btn
               block
@@ -23,44 +26,53 @@
               rounded
               class="mt-5"
               @click="demo_generate"
+              style="font-family: 'Comic Sans MS', serif"
           >Generate
           </v-btn>
-          {{"TODO: Make this output prettier"}}
-          <h3 > Output: {{output_str}} </h3>
           <v-btn
               block
               elevation="2"
               outlined
               rounded
               class="mt-5"
+              style="font-family: 'Comic Sans MS', serif"
+              @click="clearFields"
           >Clear
           </v-btn>
-
+          <v-textarea
+              ref = "OutputBox"
+              name="output"
+              v-model="output_str"
+              class="mt-5"
+              readonly
+              style="font-family: 'Comic Sans MS', serif"
+          ></v-textarea>
+        </v-col>
+<!--        Right side of the page-->
+        <v-col style="background: #CCFFFF" class="rounded-xl ml-2">
+          <v-toolbar-title style="font-size: 20px; font-family: 'Comic Sans MS',serif">3D Viewer</v-toolbar-title>
           <v-file-input
               label="Manual Upload Model Files"
               type="file"
               id="file"
               ref="myFiles"
-              class="custom-file-input"
+              class="custom-file-input mt-4 mr-4"
               accept=".obj"
               @change="previewFiles"
               multiple="true"
+              style="font-family: 'Comic Sans MS', serif"
           ></v-file-input>
+          <vue3dLoader
+              backgroundAlpha="0.3"
+              id="viewer"
+              ref="myViewer"
+              :filePath="filePath"
+              :cameraPosition="{ x: 1, y: -5, z: -20 }"
+              :height="350"
+          />
         </v-col>
-
-      </v-container>
-<!--      This is the 3D model viewer-->
-<!--          Support: obj, dae, json...-->
-<!--          Left mouse to rotate-->
-<!--          Right mouse to move-->
-<!--          Scroll to zoom in/out-->
-      <vue3dLoader
-          id="viewer"
-          ref="myViewer"
-          :filePath="filePath"
-          :cameraPosition="{ x: 1, y: -5, z: -20 }"
-          :height="350"
-      />
+      </v-row>
+    </v-col>
     </v-main>
   </v-app>
 
@@ -99,13 +111,17 @@ const api_gateway = 'http://localhost:3000' // Hardcoded, should use Environment
          console.log(this.filePath)
        }
      },
+     clearFields(){
+       this.input_str = "";
+       this.output_str = "";
+     },
 
     async demo_generate() {
-      this.output_str = "Generating ..."
-      const resolver_api = "/api/resolve_ner"
-      const url = api_gateway + resolver_api
-      let vm = this
-      await axios.post(url, {input_str: vm.input_str}).then((res) => vm.output_str = JSON.stringify(res.data))
+      this.output_str = "Generating ...";
+      const resolver_api = "/api/resolve_ner";
+      const url = api_gateway + resolver_api;
+      let vm = this;
+      await axios.post(url, {input_str: vm.input_str}).then((res) => vm.output_str = JSON.stringify(res.data));
     }
   }
  }
